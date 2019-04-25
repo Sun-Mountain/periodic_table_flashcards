fcf5fe4f2a2243d2465504f62e61489f2b021bb5
Sun-Mountain <npzonnenberg@gmail.com>
2019-04-25 09:49:47 -0400

    cycle thru periodic table

--- ptflash/src/components/Home/Home.js
+++ ptflash/src/components/Home/Home.js
@@ -4,49 +4,73 @@ import axios from 'axios';
 
 class Home extends Component {
 
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
 
         this.state = {
-            atomicNumber: '',
-            symbol: '',
-            name: ''
+            elementArray: [],
+            arrayIndex: 0,
+            currentAtomicNumber: '',
+            currentSymbol: '',
+            currentName: ''
         }
 
         this.getPeriodicTable = this.getPeriodicTable.bind(this)
+        this.addOne = this.addOne.bind(this)
     }
 
     componentDidMount() {
         this.getPeriodicTable(this.state)
-
-        console.log(this.state)
     }
 
     getPeriodicTable() {
         const Url = 'https://neelpatel05.pythonanywhere.com'
 
+        const arrayIndex = this.state.arrayIndex
+
         // fetch periodic table api in array
-        axios.get(Url)
+        axios.get(Url, arrayIndex)
             .then(res => {
                 let updateList = res.data
-                
-                let currentElement = updateList[0]
+
+                let currentElement = updateList[arrayIndex]
 
                 console.log(currentElement)
 
                 this.setState({
-                    atomicNumber: currentElement.atomicNumber,
-                    symbol: currentElement.symbol,
-                    name: currentElement.name
+                    elementArray: updateList,
+                    currentAtomicNumber: currentElement.atomicNumber,
+                    currentSymbol: currentElement.symbol,
+                    currentName: currentElement.name
                 })
 
-                console.log(this.state)
+                // console.log(this.state)
             })
 
             // console log error
             .catch(err => {
                 console.log('ERROR')
             })
+
+    }
+
+    addOne() {
+
+        this.setState(prevState => {
+            let newIndex = prevState.arrayIndex + 1
+
+            let newElement = prevState.elementArray[newIndex]
+
+            return {
+                arrayIndex: newIndex,
+                currentAtomicNumber: newElement.atomicNumber,
+                currentSymbol: newElement.symbol,
+                currentName: newElement.name
+
+            }
+        })
+        
+        console.log(this.state)
     }
 
     render() {
@@ -55,14 +79,14 @@ class Home extends Component {
             <div className="flashcard">
 
                 <div className="card">
-                    <div id="atom-num">{this.state.atomicNumber}</div>
-                    <div id="atom-sym">{this.state.symbol}</div>
-                    <div id="atom-name">{this.state.name}</div>
+                    <div id="atom-num">{this.state.currentAtomicNumber}</div>
+                    <div id="atom-sym">{this.state.currentSymbol}</div>
+                    <div id="atom-name">{this.state.currentName}</div>
                 </div>
 
-                <div className="links">
-                    <div>Previous</div>
-                    <div>Next</div>
+                <div className="buttons">
+                    <button>Previous</button>
+                    <button onClick={this.addOne}>Next</button>
                 </div>
             </div>
         )
